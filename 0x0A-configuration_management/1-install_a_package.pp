5c42ba0f271e2a9c02f46d825dc861c772df6947
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2023-03-25 00:29:32 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
 # This Puppet manifest installs puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
