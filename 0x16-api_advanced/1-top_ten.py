69207edc0e95372bc9cc577b4fae02c83aa04705
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2024-03-29 21:28:34 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,6 +3,7 @@
     Uses reddit API to get 10 hot posts
 """
 import requests
+import sys
 
 def top_ten(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
@@ -12,9 +13,18 @@ def top_ten(subreddit):
         response = requests.get(url, headers=headers, params={"limit": 10})
         response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
         data = response.json()
-        posts = data["data"]["children"]
-        for post in posts:
-            title = post["data"]["title"]
-            print(title)
+        if "data" in data and "children" in data["data"]:
+            posts = data["data"]["children"]
+            for post in posts:
+                title = post["data"]["title"]
+                print(title)
+        else:
+            print("None")
     except requests.RequestException:
         print("None")
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
