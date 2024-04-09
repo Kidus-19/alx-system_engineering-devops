70a8dce92746a7f729dfd7ef1c344bf7edd2dc72
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2024-04-10 00:11:25 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,16 +8,18 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """Read reddit API and return number subscribers """
-    username = 'ledbag123'
-    password = 'Reddit72'
-    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
-    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    client = requests.session()
-    client.headers = headers
-    r = client.get(url, allow_redirects=False)
-    if r.status_code == 200:
-        return (r.json()["data"]["subscribers"])
+    # Reddit API endpoint
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set the user-agent header
+    headers = {'User-Agent': 'YOUR_USER_AGENT'}
+
+    # Send the GET request
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['subscribers']
     else:
-        return(0)
+        return 0
