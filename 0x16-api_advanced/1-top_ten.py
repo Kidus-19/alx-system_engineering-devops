ade42eb86b4fc57d6500b46f05bd238ce63d51e6
Kidus-19 <105101940+Kidus-19@users.noreply.github.com>
2024-04-09 23:45:59 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,30 +1,25 @@
 #!/usr/bin/python3
-"""
-    Uses reddit API to get 10 hot posts
-"""
+""" Exporting csv files"""
+import json
 import requests
 import sys
 
+
 def top_ten(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-    headers = {"User-Agent": "Custom User Agent"}  # Set a custom User-Agent header
-
-    try:
-        response = requests.get(url, headers=headers, params={"limit": 10})
-        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
-        data = response.json()
-        if "data" in data and "children" in data["data"]:
-            posts = data["data"]["children"]
-            for post in posts:
-                title = post["data"]["title"]
-                print(title)
-        else:
-            print("None")
-    except requests.RequestException:
-        print("None")
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
+    """Read reddit API and return top 10 hotspots """
+    username = 'ledbag123'
+    password = 'Reddit72'
+    user_pass_dict = {'user': username, 'passwd': password, 'api_type': 'json'}
+    headers = {'user-agent': '/u/ledbag123 API Python for Holberton School'}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    client = requests.session()
+    client.headers = headers
+    r = client.get(url, allow_redirects=False)
+    if r.status_code == 200:
+        list_titles = r.json()['data']['children']
+        for a in list_titles[:10]:
+            print(a['data']['title'])
+    else:
+        return(print("None"))"Please pass an argument for the subreddit to search.")
     else:
         top_ten(sys.argv[1])
